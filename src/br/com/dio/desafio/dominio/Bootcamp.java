344f53544cf8f8ea6f75c0b7128a9f39abb1d985
nataliak-kikuti <nataliakikuti@gmail.com>
2022-10-12 16:54:36 -0300

    Final

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,68 @@
+package br.com.dio.desafio.dominio;
+import java.util.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public String toString() {
+        return "BootCamp{" +
+                "Nome BootCamp = '" + nome + '\'' +
+                ",\nDescrição BootCamp = '" + descricao + '\'' +
+                ",\nData Inicial = " + dataInicial +
+                ",\nData Final = " + dataFinal +
+                ",\nDevs Inscritos = " + devsInscritos +
+                ",\nConteúdos = " + conteudos +
+                '}';
+    }
+}
